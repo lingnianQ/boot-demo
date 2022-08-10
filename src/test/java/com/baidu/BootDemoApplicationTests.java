ec90e24b51d6c40e06b084ce8b0b08eb588dadda
lingnian <sytsnb@gmail.com>
2022-08-10 09:46:56 +0800

    Initial commit

---
+++ src/test/java/com/baidu/BootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
