73839eac7e265f298c6dba7cf50bd67501f6b5f3
lingnian <sytsnb@gmail.com>
2022-08-10 11:09:11 +0800

    index

--- src/test/java/com/baidu/BootDemoApplicationTests.java
+++ src/test/java/com/baidu/BootDemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.baidu;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootDemoApplicationTests {
+//    @Test
+//    public static void main(String[] args) {
+//        SpringApplication.run(BootDemoApplication.class, args);
+//    }
 
     @Test
     void contextLoads() {
+
     }
 
 }
